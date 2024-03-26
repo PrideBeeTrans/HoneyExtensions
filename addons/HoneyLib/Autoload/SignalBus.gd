97baca15855e84a2b8b612eb7019bc231da07cbf
Hive Bee <132298245+PrideBeeTrans@users.noreply.github.com>
2024-03-26 09:43:28 -0300

    Improvements To SignalBus

--- addons/HoneyLib/Autoload/SignalBus.gd
+++ addons/HoneyLib/Autoload/SignalBus.gd
@@ -34,40 +34,40 @@ func get_listener_object(event_name: String,index: int) -> Object:
 	return get_listener(event_name, index)["object"]
 
 
-func remove_event(event_name: String, callback: String, object: Object) -> void:
-	if has_event(event_name):
-		var callbacks := get_event(event_name) as Array
-		for index in range(callbacks.size()):
-			var entry := get_listener(event_name, index) as Dictionary
-			var entry_callback := get_listener_callback(event_name, index)
-			var entry_object := get_listener_object(event_name, index)
-			
-			if entry_callback == callback and entry_object == object:
-				callbacks.remove(index)
-				return
-
-
-func emit_event(event_name: String, args: Dictionary = {}) -> void:
-	if has_event(event_name):
-		var callbacks := get_event(event_name) as Array
-		if callbacks.size() > MIN_INDEX:
-			for index in callbacks.size():
-				var callback := get_listener_callback(event_name, index)
-				var object := get_listener_object(event_name, index)
-				if args.size() > EMPTY_INDEX:
-					object.call(callback, args)
-				else:
-					object.call(callback)
-		else:
-			var callback := get_listener_callback(event_name, START_INDEX) as String
-			var object := get_listener_object(event_name, START_INDEX) as Object
-			if args.size() > 0:
-				object.call(callback, args)
-			else:
-				object.call(callback)
-			
-	else:
+func remove_event(event_name: String,callback: String, object: Object) -> void:
+	if not has_event(event_name):
 		push_error("No event with this name %s" % event_name)
+		return
+	var callbacks := get_event(event_name) as Array
+	for index in callbacks.size():
+		var entry := get_listener(event_name, index) as Dictionary
+		var entry_callback := get_listener_callback(event_name, index)
+		var entry_object := get_listener_object(event_name, index)
+		
+		if entry_callback == callback and entry_object == object:
+			callbacks.remove(index)
+			return
+
+
+func emit_event(event_name: String,args: Dictionary = {}) -> void:
+	if not has_event(event_name):
+		push_error("No event with this name %s" % event_name)
+		return
+	var callbacks := get_event(event_name) as Array
+	if callbacks.size() > MIN_INDEX:
+		for index in callbacks.size():
+			call_listener(event_name, index, args)
+	else:
+		call_listener(event_name, START_INDEX, args)
+
+
+func call_listener(event_name: String,index: int,args: Dictionary = {}) -> void:
+	var callback := get_listener_callback(event_name, index)
+	var object := get_listener_object(event_name, index)
+	if args.size() > EMPTY_INDEX:
+		object.call(callback, args)
+	else:
+		object.call(callback)
 
 
 func has_event(event_name: String) -> bool:
