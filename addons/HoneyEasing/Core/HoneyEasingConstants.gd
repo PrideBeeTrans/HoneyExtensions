d6a1d83626421092c7832a80e6317d6b7baf9d45
Hive Bee <132298245+PrideBeeTrans@users.noreply.github.com>
2024-04-08 15:52:09 -0300

    Delete addons/HoneyEasing directory

--- addons/HoneyEasing/Core/HoneyEasingConstants.gd
+++
@@ -1,9 +0,0 @@
-class_name HoneyEasingConstants
-extends Reference
-
-const LoopTypes := {
-	"Restart":"Restart",
-	"YoyoYoyo":"YoyoYoyo",
-	"PingPong":"PingPong",
-	"BackAndForth":"BackAndForth",
-}
--- addons/HoneyEasing/Core/HoneyEasingNode.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-
-func _ready() -> void:
-	var honey_easing := HoneyEasing.create_honey_easing(self)
-	honey_easing.ease_property($Sprite, "position", Vector2(200,200), 0.5)
